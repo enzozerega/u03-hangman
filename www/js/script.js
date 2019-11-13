0a6f48751aaa4258b4c9a242adfef7ff9d0c14fb
enzozerega <enzo.zerega@chasacademy.se>
2019-11-13 11:28:05 +0100

    Solve dynamic keys

--- www/js/script.js
+++ www/js/script.js
@@ -1,19 +1,35 @@
 let keyLetter = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
 
-for (i=0; i < keyLetter.length; i++) {
+let i = 0; 
+do { 
+  task(i); 
+  i++; 
+} while (i < keyLetter.length); 
+function task(i) { 
+  setTimeout(function() { 
+     
     let keyBox = document.createElement('div');
-    keyBox.className = 'key-box';
+    keyBox.className = 'key-box faded-out';
     let keySpan = document.createElement('span');
     keySpan.className = 'key-letter';
     keySpan.innerHTML = keyLetter[i];
     document.getElementById('keyboard').appendChild(keyBox);
     keyBox.appendChild(keySpan);
+
     if (keyLetter[i] === 'P'|| keyLetter[i] === 'L') {
         let keyBreak = document.createElement('div');
         keyBreak.className = 'key-break';
         document.getElementById('keyboard').appendChild(keyBreak);
     }
-}
+
+    requestAnimationFrame(() => {
+        keyBox.classList.remove("faded-out")
+      });
+
+  }, 300 * i); 
+};
+
+
 
 let words = ['Blue', 'Green', 'Yellow', 'Pink'];
 let indexRandom = Math.floor(Math.random() * words.length);
